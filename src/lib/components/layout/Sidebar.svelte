6402abc3519c044fcfd33bbb25acea31dff3d04b
Tanvir <operaous@gmail.com>
2026-06-27 17:56:53 +0000

    Sidebar: active row #212121, inner rows rounded-xl to nest in the box

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -1070,7 +1070,7 @@
 					<div class="px-[0.4375rem] flex justify-center text-gray-800 dark:text-gray-200">
 						<a
 							id="sidebar-new-chat-button"
-							class="group grow flex items-center space-x-3 rounded-2xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition outline-none {sbPath === '/' ? 'bg-black/5 dark:bg-white/[0.08]' : ''}\"
+							class="group grow flex items-center space-x-3 rounded-xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition outline-none {sbPath === '/' ? 'bg-black/5 dark:bg-[#212121]' : ''}\"
 							href="/"
 							draggable="false"
 							on:click={newChatHandler}
@@ -1091,7 +1091,7 @@
 					<div class="px-[0.4375rem] flex justify-center text-gray-800 dark:text-gray-200">
 						<button
 							id="sidebar-search-button"
-							class="group grow flex items-center space-x-3 rounded-2xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition outline-none"
+							class="group grow flex items-center space-x-3 rounded-xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition outline-none"
 							on:click={() => {
 								showSearch.set(true);
 							}}
@@ -1119,7 +1119,7 @@
 								>
 									<a
 										id="sidebar-{itemId}-button"
-										class="grow flex items-center space-x-3 rounded-2xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition {sbPath === meta.href ? 'bg-black/5 dark:bg-white/[0.08]' : ''}\"
+										class="grow flex items-center space-x-3 rounded-xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition {sbPath === meta.href ? 'bg-black/5 dark:bg-[#212121]' : ''}\"
 										href={meta.href}
 										on:click={itemClickHandler}
 										draggable="false"
